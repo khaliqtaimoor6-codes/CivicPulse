1817f5c8f0596f2f6dc99c6b1393e760bd828b54
khaliqtaimoor6-codes <khaliqtaimoor6@gmail.com>
2026-09-25 15:30:16 +0500

    feat: add frontend routing and compose stack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,25 @@
-// Placeholder.
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+
+import DashboardPage from "./pages/DashboardPage";
+import StatsPage from "./pages/StatsPage";
+import SubmitPage from "./pages/SubmitPage";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav aria-label="Main navigation">
+        <NavLink to="/">Submit</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/stats">Stats</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SubmitPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
